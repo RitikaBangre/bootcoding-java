04c16eb8750562b91ae3800f408a73a58878f932
RitikaBangre <ritikabangre2402@gmail.com>
2023-04-27 18:15:54 +0530

    Arithmetic examples

---
+++ src/main/java/com/bootcoding/java/basic/ArithmeticEx3.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.basic;
+
+public class ArithmeticEx3 {
+    public static void main(String[] data) {
+        int a = 10;
+        int b = 5;
+        int res = a * b + a / b + a * b;
+        System.out.print(res);
+    }
+}
