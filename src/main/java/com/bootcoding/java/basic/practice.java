2ebaec947028b8b88e03fda3f44995b2d39f7f73
RitikaBangre <ritikabangre2402@gmail.com>
2023-05-02 23:00:34 +0530

    Basic programs

---
+++ src/main/java/com/bootcoding/java/basic/practice.java
@@ -0,0 +1,21 @@
+package com.bootcoding.java.basic;
+
+public class practice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int Add = 0;
+        int Sub = 0;
+        int Mult = 0;
+        int Div = 0;
+
+        Add = a + b;
+        Sub = a - b;
+        Mult = a * b;
+        Div = a / b;
+        System.out.println(Add);
+        System.out.println(Sub);
+        System.out.println(Mult);
+        System.out.println(Div);
+    }
+}
---
+++ src/main/java/com/bootcoding/java/basic/practiceEx1.java
@@ -0,0 +1,21 @@
+package com.bootcoding.java.basic;
+
+public class practiceEx1{
+ public static void main(String[] args){
+ int a=20;
+ int b=5;
+ int c=a+b;
+ int d=a-b;
+ int e=a*b;
+ int f=a/b;
+ int g = a%b ;
+ 
+ System.out.println("Addition of a+b:  is" + c);
+ System.out.println("Substraction of a-b:  is" + d);
+ System.out.println( "Multiplication of a*b: is" + e);
+ System.out.println("Division of a/b: is" + f);
+ System.out.println("Module of a%b: is " +g);
+     }
+     
+   }
+  
