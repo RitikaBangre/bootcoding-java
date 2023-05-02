ceba34032b39848a27d66ca476d663f556295c70
RitikaBangre <ritikabangre2402@gmail.com>
2023-05-02 22:39:29 +0530

    Basic programs on even and odd number by CLA

---
+++ src/main/java/com/bootcoding/java/basic/Example5.java
@@ -0,0 +1,19 @@
+package com.bootcoding.java.basic;
+
+public class Example5{
+
+public static void main(String[]value){
+
+  
+  int n=Integer.parseInt(value[0]);
+    n=Integer.parseInt(value[1]);
+
+  if (n %2==0){
+  System.out.println(n+  "is even");
+  }
+   else{
+   System.out.println(n+ "is odd");
+   }
+}
+}	
+
---
+++ src/main/java/com/bootcoding/java/basic/cla.java
@@ -0,0 +1,26 @@
+package com.bootcoding.java.basic;
+
+public class cla{
+
+public static void main(String[] value){
+
+  int n=Integer.parseInt(value[0]);
+  int m=Integer.parseInt(value[1]);
+  //String value=data[0];
+  //String value=data[1];
+  
+  if (n%2==0){
+  System.out.println(n+  "is even");
+  }
+   else{
+   System.out.println(n+ "is odd");
+   }
+   if(m%2==0){
+	   System.out.println(m+ "is even");
+   }
+    else{
+		System.out.println(m+ "is odd");
+	}
+}
+ }	
+
